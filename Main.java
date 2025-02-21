0e5854dc8589d6fabe993eab4c9771683d47e127
Filipi Pereira <filipiacademico@gmail.com>
2025-02-21 13:34:17 -0300

    Adicionei

--- Main.java
+++ Main.java
@@ -1,98 +1,152 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-class Filme {
-    private String nome;
-    private LocalDate data;
-    private int investimento;
-    private float bilheteira;
-    private String genero;
-    private List<String> financiadores;
+class Film {
+    private String name;
+    private long date;
+    private int budget;
+    private float boxOffice;
+    private String genre;
+    private List<String> financingCompanies;
 
-    public Filme() {
-        this.nome = null;
-        this.data = null;
-        this.investimento = 0;
-        this.bilheteira = 0;
-        this.financiadores = null;
-        this.genero = null;
+    public Film() {
+        this.name = null;
+        this.date = -1;
+        this.budget = -1;
+        this.boxOffice = -1;
+        this.financingCompanies = null;
+        this.genre = null;
     }
 
-    public Filme(String nome, LocalDate data, int investimento, float bilheteira, List<String> financiadores, String genero) {
-        this.nome = nome;
-        this.data = data;
-        this.investimento = investimento;
-        this.bilheteira = bilheteira;
-        this.financiadores = financiadores;
-        this.genero = genero;
+    public Film(String name, long date, int budget, float boxOffice, List<String> financingCompanies, String genre) {
+        this.name = name;
+        this.date = date;
+        this.budget = budget;
+        this.boxOffice = boxOffice;
+        this.financingCompanies = financingCompanies;
+        this.genre = genre;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public LocalDate getData() {
-        return data;
+    public long getDate() {
+        return date;
     }
 
-    public void setData(LocalDate data) {
-        this.data = data;
+    public void setDate(long date) {
+        this.date = date;
     }
 
-    public int getInvestimento() {
-        return investimento;
+    public int getBudget() {
+        return budget;
     }
 
-    public void setInvestimento(int investimento) {
-        this.investimento = investimento;
+    public void setBudget(int budget) {
+        this.budget = budget;
     }
 
-    public float getBilheteira() {
-        return bilheteira;
+    public float getBoxOffice() {
+        return boxOffice;
     }
 
-    public void setBilheteira(float bilheteira) {
-        this.bilheteira = bilheteira;
+    public void setBoxOffice(float boxOffice) {
+        this.boxOffice = boxOffice;
     }
 
-    public List<String> getfinanciadores() {
-        return financiadores;
+    public List<String> getFinancingCompanies() {
+        return financingCompanies;
     }
 
-    public void setfinanciadores(List<String> financiadores) {
-        this.financiadores = financiadores;
+    public void setFinancingCompanies(List<String> financingCompanies) {
+        this.financingCompanies = financingCompanies;
     }
 
-    public String getGenero() {
-        return genero;
+    public String getGenre() {
+        return genre;
     }
 
-    public void setGenero(String genero) {
-        this.genero = genero;
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 
+    public int byteSize(){
+        int size = 0;
+        size += 2 + name.getBytes().length; // name; writeUTF() uses 2 bytes for the size of the string
+        size += 8; // date
+        size += 4; //budget 
+        size += 4; //boxOffice 
+        size += 10; //genre; fixed-size string
+        //list of financingCompanies
+        size += 4; // size of list
+        for (String fc : financingCompanies) {
+            size += 2 + fc.getBytes().length; // financingCompanies
+        }
+        //endlist
+        return size;
+    }
+}
 
+class LoadCsv{
+    public void LoadFromCsv(){
+        String linha;
+        SequentialFile sequentialFile = new SequentialFile();
+        try(FileReader fileReader = new FileReader("filmsDataSet.csv")){
+            BufferedReader br = new BufferedReader(fileReader);
+            linha = br.readLine();//ignores first line
+            while(br.readLine() != null){
+                String[] values = linha.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                String name = values[0];
+                LocalDate date = LocalDate.parse(values[1]);
+                long epochDate = date.atStartOfDay(ZoneOffset.UTC).toEpochSecond();
+                int budget = Integer.parseInt(values[2]);
+                float boxOffice = Float.parseFloat(values[3]);
+                String genre = values[4];
+                List<String> financingCompanies = new ArrayList<>(Arrays.asList(values[5].replace("\"", "").split(",")));
+                Film film = new Film(name, epochDate, budget, boxOffice, financingCompanies, genre);
+                sequentialFile.Insert(film);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
 
+class SequentialFile{
+    private String name;
+    public SequentialFile(){
+        this.name = "SequentialFile.dat";
+    }
+    public boolean Insert(Film film){
+        try (RandomAccessFile file = new RandomAccessFile(name, "w")) {
+            file.seek(0);
+            int lastId = file.readInt();
+            int objectId = lastId + 1;
+            file.seek(0);
+            file.writeInt(objectId);
+            file.seek(file.length());
+            file.writeByte(0);//flag
+            file.writeInt(film.byteSize());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 public class Main {
     public static void main(String[] args) {
-      //  Scanner scanner = new Scanner(System.in);
-
-        LocalDateTime data = LocalDateTime.of(2025, Month.FEBRUARY, 21, 12, 0, 0, 0);
-        LocalDate epoch = data.toInstant(ZoneOffset.UTC).toEpochMilli();
-
-        System.out.println("Timestamp epoch (milli): " + epoch);
-
-        Instant instant = Instant.ofEpochSecond(epoch);
-        ZonedDateTime dateTime = instant.atZone(ZoneOffset.UTC);
-
-        System.out.println("Data e hora correspondente ao timestamp: " + dateTime);
-
-
-       // scanner.close();
+        
     }
 }
