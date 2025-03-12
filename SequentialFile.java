f0267de66f45dfe52aaaf39c731652ee354b91e3
Filipi Pereira <filipiacademico@gmail.com>
2025-03-12 20:35:40 -0300

    melhorei intercalacao

--- SequentialFile.java
+++ SequentialFile.java
@@ -4,7 +4,6 @@
  */
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 public class SequentialFile {
     private static String FILE_NAME = "SequentialFile.dat";
@@ -217,6 +216,35 @@ public class SequentialFile {
         QuickSort(array, 0, array.length-1);
     }
 
+    public static void QuickSort(ArrayList<Film> arrayList, int esq, int dir){
+        int i = esq;
+        int j = dir;
+        Film pivo = arrayList.get((i + j)/2);
+        while(i <= j){
+            while(pivo.getId() > arrayList.get(i).getId()){
+                i++;
+            }
+            while(pivo.getId() < arrayList.get(j).getId()){
+                j--;
+            }
+            if(i <= j){
+                swap(arrayList, i, j);
+                i++;
+                j--;
+            }
+        }
+        if(esq < j) QuickSort(arrayList, esq, j);
+        if(i < dir) QuickSort(arrayList, i, dir);
+    }
+    public static void swap(ArrayList<Film> arrayList,int a, int b){
+        Film temp = arrayList.get(b);
+        arrayList.set(b, arrayList.get(a));
+        arrayList.set(a, temp);
+    }
+    public static void QuickSort(ArrayList<Film> arrayList){
+        QuickSort(arrayList, 0, arrayList.size()-1);
+    }
+
     public static void ExternalSort(int b, int m){
         //Distribution
         int totalRecords = 0;
@@ -253,7 +281,7 @@ public class SequentialFile {
                                     lastBlockFilm.add(film);
                                 }
                             }
-                            lastBlockFilm.sort(Comparator.comparingInt(Film::getId));
+                            QuickSort(lastBlockFilm);
                             for(int i = 0; i < lastBlockFilm.size(); i++){
                                 tempFile.seek(tempFile.length());
                                 WriteFilm(tempFile, lastBlockFilm.get(i));
@@ -276,79 +304,129 @@ public class SequentialFile {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Intercalation(m, b, totalRecords, "TempFile", "TempFileTwo");
+        Intercalation(m, b, totalRecords);
     }
 
-    public static void Intercalation(int currentNumberOfFiles, int b, int totalRecords, String fileName1, String fileName2){
-        System.out.println("Entrei na funcao");
-        if(currentNumberOfFiles > 1){
-            int nextNumberOfFiles = (int)Math.ceil(totalRecords/(currentNumberOfFiles*b)) < currentNumberOfFiles ? (int)Math.ceil(totalRecords/(currentNumberOfFiles*b)) : currentNumberOfFiles;
-            System.out.println("Current number of files:" + currentNumberOfFiles);
-            //positionsTempFile
-            long positionTempFile[] = new long[currentNumberOfFiles];
-            for(int i = 0; i < currentNumberOfFiles; i++){
-                positionTempFile[i] = 0;
+    public static void Intercalation(int m, int b, int registrosTotais){
+        System.out.println(registrosTotais);
+        int numerosDeArquivos = m;
+        int quantidadeRegistrosPorBloco = b;
+        int numeroDaIntercalacao = 0;
+        while(numerosDeArquivos > 1){
+            
+            //controla os nomes dos arquivos(par ou impar)
+            numeroDaIntercalacao++;
+            String arquivosAtuais;
+            String arquivosProximos;
+            if(numeroDaIntercalacao % 2 == 1){
+                arquivosAtuais = "TempFile";
+                arquivosProximos = "TempFileAux";
+            } 
+            else{
+                arquivosAtuais = "TempFileAux";
+                arquivosProximos = "TempFile";
             }
-            //positionsTempFile2
-            long positionTempFile2[] = new long[nextNumberOfFiles];
-            for(int i = 0; i < nextNumberOfFiles; i++){
-                positionTempFile2[i] = 0;
+
+            //proximo numero de arquivos calculo
+            int proximoNumeroDeArquivos = 0;
+            int calculo = (registrosTotais / (quantidadeRegistrosPorBloco * numerosDeArquivos));
+            if(calculo >= numerosDeArquivos){
+                proximoNumeroDeArquivos = numerosDeArquivos;
             }
-            int tRecords = 0;
-            while(tRecords < totalRecords){
-                System.out.println("quant registros" + tRecords);
-                for(int paths = 0; paths < nextNumberOfFiles && tRecords < totalRecords; paths++){
-                    System.out.println("caminho" + paths);
-                    int quantity[] = new int[currentNumberOfFiles];
-                    for(int i = 0; i < currentNumberOfFiles; i++){
-                        quantity[i] = 0;
-                    }
-                    for(int records = 0; records < currentNumberOfFiles*b && tRecords < totalRecords; records++){
-                        int smallerId = Integer.MAX_VALUE;
-                        int smallestidPath = -1;
-                        for (int i = 0; i < currentNumberOfFiles && tRecords < totalRecords; i++) {
-                            if (quantity[i] < b) {
-                                try (RandomAccessFile tempFile = new RandomAccessFile(fileName1 + (i + 1), "rw")) {
-                                    if (positionTempFile[i] < tempFile.length()) {  //
-                                        tempFile.seek(positionTempFile[i] + 5);
-                                        int id = tempFile.readInt();
-                                        if (id < smallerId) {
-                                            smallerId = id;
-                                            smallestidPath = i;
-                                        }
+            else if((registrosTotais % (quantidadeRegistrosPorBloco * numerosDeArquivos)) == 0){
+                proximoNumeroDeArquivos = (registrosTotais / (quantidadeRegistrosPorBloco * numerosDeArquivos));
+            }   
+            else{
+                proximoNumeroDeArquivos = calculo + 1;
+            }
+            System.out.println("numeros arquivos atuais: " + numerosDeArquivos);
+
+            //iniciailizdo vetores que guardam os ponteiro dos arquivos
+            long posicoesAtual[] = new long[numerosDeArquivos];
+            for(int i = 0; i < numerosDeArquivos; i++){
+                posicoesAtual[i] = 0;
+            }
+            long posicoesProximo[] = new long[proximoNumeroDeArquivos];
+            for(int i = 0; i < proximoNumeroDeArquivos; i++){
+                posicoesProximo[i] = 0;
+            }
+            //para cada intercalacao le se o numero total de registros
+            int totaisRegistrosLidos = 0;
+            boolean acabouIntercalacao = false;
+            while(totaisRegistrosLidos < registrosTotais && !acabouIntercalacao){
+                
+                //cada secao vai ser escrita em um caminho diferente
+                int quantidadeCaminhosProximo = 0;                
+                while(quantidadeCaminhosProximo < proximoNumeroDeArquivos && !acabouIntercalacao){
+
+                    //para cada parte da intercalacao le se o numero de registros da secao
+                    int registrosLidosPorSecao = 0;
+                    while(registrosLidosPorSecao < numerosDeArquivos*quantidadeRegistrosPorBloco  && !acabouIntercalacao){
+
+                        //cada arquivo so pode ser lido em blocos
+                        //inicializacao do vetor que controla ate onde pode ser lido
+                        int quantidadeDeLeiturasPorArquivoAtual[] = new int[numerosDeArquivos];
+                        for(int i = 0; i < numerosDeArquivos; i++){
+                            quantidadeDeLeiturasPorArquivoAtual[i] = 0;
+                        }
+
+                        //iniciando as leituras para achar o arquivo que possui o menor id
+                        int menorId = Integer.MAX_VALUE;
+                        int ArquivoMenorId = -1;
+                        for (int i = 0; i < numerosDeArquivos; i++) {
+                            try (RandomAccessFile arquivoAtual = new RandomAccessFile(arquivosAtuais + (i + 1), "rw")) {
+                                arquivoAtual.seek(posicoesAtual[i] + 5);
+                                //condicoes para leitura, o arquivo nao ter lido o bloco inteiro e o arquivo nao tiver acabado
+                                if (quantidadeDeLeiturasPorArquivoAtual[i] < quantidadeRegistrosPorBloco && arquivoAtual.getFilePointer() < arquivoAtual.length()){
+                                    int id = arquivoAtual.readInt();
+                                    if (id < menorId) {
+                                        menorId = id;
+                                        ArquivoMenorId = i;
                                     }
-                                } catch (Exception e) {
-                                    e.printStackTrace();
                                 }
-                            }
-                        }
-                        System.out.println("posicao menor id: " + smallestidPath);
-                        Film film = null;
-                        if (smallestidPath != -1) {
-                            try (RandomAccessFile tempFile = new RandomAccessFile(fileName1 + (smallestidPath+1), "rw")) {
-                                tempFile.seek(positionTempFile[smallestidPath] + 5);//5 = byte flag + register size;
-                                film = ReadFilm(tempFile);
-                                positionTempFile[smallestidPath] += 5 + film.registerByteSize();
-                                quantity[smallestidPath]++;
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
                         }
-                        if(film != null){
-                            try (RandomAccessFile tempFile = new RandomAccessFile(fileName2 + (paths+1), "rw")) {
-                                tempFile.seek(positionTempFile2[paths]);
-                                WriteFilm(tempFile, film);
-                                tRecords++;
-                                positionTempFile2[paths] += 5 + film.registerByteSize(); 
+                        //ler o filme de menor id e marcar a posicao do arquivo
+                        Film filme = null;
+                        if(ArquivoMenorId != -1){
+                            try (RandomAccessFile arquivoAtual = new RandomAccessFile(arquivosAtuais + (ArquivoMenorId+1), "rw")) {
+                                arquivoAtual.seek(posicoesAtual[ArquivoMenorId] + 5);//5 = byte flag + register size;
+                                if(arquivoAtual.getFilePointer() < arquivoAtual.length()){
+                                    filme = ReadFilm(arquivoAtual);
+                                    posicoesAtual[ArquivoMenorId] += 5 + filme.registerByteSize();
+                                    quantidadeDeLeiturasPorArquivoAtual[ArquivoMenorId]++;
+                                }
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
                         }
-                        System.out.println("records: " + records);
+                        //escrever o filme no arquivo proximo na posicao quantidade caminhos proximos
+
+                        if(filme != null){
+                            try (RandomAccessFile arquivoProximo = new RandomAccessFile(arquivosProximos + (quantidadeCaminhosProximo+1), "rw")) {
+                                arquivoProximo.seek(posicoesProximo[quantidadeCaminhosProximo]);
+                                WriteFilm(arquivoProximo, filme);
+                                posicoesProximo[quantidadeCaminhosProximo] += 5 + filme.registerByteSize(); 
+                                //aumentando um no contador de registros
+                                registrosLidosPorSecao++;   
+                                totaisRegistrosLidos++;
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        else acabouIntercalacao = true;
                     }
+
+                    quantidadeCaminhosProximo++;
+                    System.out.println("secao: " + registrosLidosPorSecao);
                 }
             }
-            Intercalation(nextNumberOfFiles, currentNumberOfFiles*b, totalRecords, fileName2, fileName1);
+            System.out.println("total: " + totaisRegistrosLidos);
+            quantidadeRegistrosPorBloco *= numerosDeArquivos;
+            numerosDeArquivos = proximoNumeroDeArquivos;
         }
+        System.out.println("intercalacao: " + numeroDaIntercalacao);
     }
 }
